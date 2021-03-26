6fd298250eaa325c009743cdb4b96967a82fdf27
Sinethemba Sithela <80489764+Djkingpin-10477@users.noreply.github.com>
2021-03-26 12:56:09 +0000

    Update WilburTestApp2.java

--- src/main/java/za/ac/cput/test2/WilburTestApp2.java
+++ src/main/java/za/ac/cput/test2/WilburTestApp2.java
@@ -10,5 +10,9 @@ package za.ac.cput.test2;
  * @author Wilbur Smith
  */
 public class WilburTestApp2 {
-    
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
 }
+
+
