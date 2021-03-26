2d7277958c48b174c732faf9e940f592a7a3f9d9
Alizwa Solibanzi <220001499@mycput.ac.za>
2021-03-26 17:28:27 +0200

    added a new java file "AlizwaTestApp2" and created a branch "220001499Alizwa"

---
+++ src/main/java/za/ac/cput/test2/AlizwaTestApp2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.test2;
+
+/**
+ *
+ * @author Alizwa Solibanzi
+ */
+public class AlizwaTestApp2
+{
+ int a;
+ int b;
+ AlizwaTestApp2(int a)
+ {
+   this.a = a;
+   if (this.a !=b)
+    System.out.println( b);
+ }
+ public int  testapp()
+ {
+  return a;   
+ }
+}
